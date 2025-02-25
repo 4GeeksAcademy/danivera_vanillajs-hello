7d15a6e60c2f3a063d8eab9d2ac19f89fe796a89
DaniVera-tech <daniss.vera@gmail.com>
2025-02-25 15:51:18 +0000

    danivera first commit

--- src/app.js
+++ src/app.js
@@ -7,5 +7,14 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
-};
+  let pronoun = ['the', 'our'];
+  let adj = ['great', 'big'];
+  let noun = ['jogger', 'racoon'];
+  
+  for (let p of pronoun) {
+    for (let a of adj) {
+      for (let n of noun) {
+        console.log(p + a + n + '.com');
+      }
+    }
+  }
